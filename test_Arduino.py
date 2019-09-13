74d2a9889f4c7be493606ffba90b3daa33167c38
ShaunLWM <shaunlimweimin@gmail.com>
2019-09-13 09:16:59 +0800

    Arduino sample server

---
+++ test_Arduino.py
@@ -0,0 +1,16 @@
+from module.Serial import *
+
+if __name__ == '__main__':
+    serial = Serial()
+    serial.connect()
+    print("[@] Arduino connection successfully established")
+    try:
+        msg = input("Enter msg to send: ")
+        print("[@] Writing to Arduino: {}".format(msg))
+        serial.write(msg)
+        msg = serial.read()
+        print("[@] Got from Arduino: {}".format(msg))
+        print("[@] Closing socket.")
+        serial.close()
+    except Exception as e:
+        print(e)
